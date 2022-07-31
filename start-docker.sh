c03a34007d961cdc6e040aa31eea083ee445ac69
rianmachado <rianmachado@gmail.com>
2022-07-30 22:24:11 -0300

    Ajuste na estrutura do projeto

---
+++ start-docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#####################################################################
+INICIO .... Consumindo mensagens.				    				#	   
+								   	 								#
+#####################################################################
+
+docker run  -p 8080:8080 rianmachado/quarkus-consumer-reactive-messaging-kafka:v1
+
+
+
